af36c0691458d858b15dc906386d4f954ad2d839
Yi Zhao <yizhao.cs@gmail.com>
2016-10-17 14:55:58 -0700

    3

--- src/com/yizhao/services/dataservice/RefreshDataService.java
+++ src/com/yizhao/services/dataservice/RefreshDataService.java
@@ -83,6 +83,32 @@ public class RefreshDataService extends BaseDataService {
             String DATA_PROVIDERS_SQL = "select id, name, do_pixel, ifnull(has_offline_data, 0) as has_offline_data, sync_facebook from data_providers";
             List<Map<String, Object>> rows = this
                     .getResults(DATA_PROVIDERS_SQL);
+
+/*
+            CREATE TABLE `data_providers` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `name` varchar(64) NOT NULL,
+            `do_pixel` tinyint(4) NOT NULL DEFAULT '1',
+            `has_offline_data` tinyint(4) DEFAULT NULL COMMENT '1 if dp provides offline data',
+            `is_partner` tinyint(1) NOT NULL DEFAULT '0',
+            `is_advertiser` tinyint(1) NOT NULL DEFAULT '0',
+            `region_id` int(11) DEFAULT NULL,
+            `timezone` varchar(60) NOT NULL,
+            `auto_approved` tinyint(1) NOT NULL DEFAULT '0',
+            `status` char(1) DEFAULT NULL,
+            `category_id` int(11) DEFAULT NULL,
+            `currency` varchar(3) DEFAULT NULL,
+            `europe_approved` tinyint(1) NOT NULL DEFAULT '0',
+            `mobile_partner` tinyint(1) NOT NULL DEFAULT '0',
+            `sync_facebook` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'whether this data provider wants to sync data with facebook',
+            `modification_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`),
+            KEY `modification_ts` (`modification_ts`),
+            KEY `timezone` (`timezone`),
+            KEY `name` (`name`),
+            CONSTRAINT `data_providers_ibfk_1` FOREIGN KEY (`timezone`) REFERENCES `timezones` (`name`)
+            )
+*/
             for (Map<String, Object> row : rows) {
                 DataProvider dp = new DataProvider();
                 dp.setId((Integer) row.get("id"));
